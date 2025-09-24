b2d273246ef8c8343701a6d7b8c1682c1f167fb9
pangestuasli <pangestu24si@mahasiswa.pcr.ac.id>
2025-09-24 08:07:22 +0700

    Starter Projek Laravel

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/pcr', function () {
+    return 'Selamat Datang di Website Kampus PCR!';
+});
+
+Route::get('/mahasiswa/{param1}',[MahasiswaController::class,'show']);
+
+Route::get('/nama/{param1?}', function ($param1 = ' ') {
+    return 'Nama saya: '.$param1;
+});
+
+Route::get('/nim/{param1?}', function ($param1 = '') {
+    return 'NIM saya: '.$param1;
+});
+
+Route::get('/namalengkap/{param1?}/{param2?}', function ($param1 = '', $param2 = '') {
+    return 'Nama saya: '.$param1.  '<br>NIM : ' .$param2;
+});
+
+Route::get('/about', function () {
+    return view('halaman-about');
+});
+
+Route::get('/matakuliah/{param1}',[MatakuliahController::class,'show']);
